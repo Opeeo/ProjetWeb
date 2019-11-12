5a7ddbb8881df9cc0d41853d21d530337f43cd64
SlyverStorm <44494841+SlyverStorm@users.noreply.github.com>
2019-11-12 11:45:31 +0100

    Ajout de la navbar

---
+++ navbar/navbar.php
@@ -0,0 +1,96 @@
+<nav style="background-color: black; padding-top: 0px; padding-bottom: 0px" class="navbar navbar-expand-lg navbar-custom navbar-dark fixed-top">
+    <a style="margin-right: 50px" class="navbar-brand" href="#">
+        <img src="assets/image/cesilogo.jpg" height="40">
+        <span style="margin-left: 10px">BDE - Toulouse</span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Boutique</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Evénement
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Evénement à venir</a>
+                    <a class="dropdown-item" href="#">Evénement passés</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="navbar-text">
+        <li class="register">
+            <span style="display: inline-block">
+            <a class="nav-link" href="index.php">
+                Register
+            </a>
+        </span>
+        </li>
+        <li class="login">
+            <span style="display: inline-block">
+            <a class="nav-link" href="index.php">
+                Log in
+            </a>
+        </span>
+        </li>
+
+
+
+
+    </div>
+</nav>
+
+
+
+
+
+
+
+<?php
+
+/*
+<nav style="background-color: black" class="navbar navbar-expand-md navbar-dark fixed-top">
+
+    <div class="navbar-brand">
+        <a class="navbar-brand" href="index.php"><img src="https://ecole-ingenieurs.cesi.fr/wp-content/uploads/sites/5/2018/10/logo-cesi-ecole-ingenieurs.png"></a>
+    </div>
+
+    <div class="navbar-content-area">
+        <span class="navbar-content-item">
+            Home
+        </span>
+
+        <span class="navbar-content-item">
+            Event
+        </span>
+
+        <span class="navbar-content-item">
+            Shop
+        </span>
+    </div>
+
+    <div class="navbar-content-connect">
+        <span class="navbar-content-register">
+            Register
+        </span>
+
+        <span class="navbar-content-login">
+            Log in
+        </span>
+    </div>
+
+
+
+
+
+</nav>*/
+
+?>
