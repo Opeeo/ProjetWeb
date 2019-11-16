d00ebc6fba9b49c5bb643601e79f346373d7b337
Ypsillonn <35596935+Ypsillonn@users.noreply.github.com>
2019-11-16 10:40:51 +0100

    Accés à la page de déconnexion interdit si pas connecté

--- ProjetWeb/content/deconnexion.php
+++ ProjetWeb/content/deconnexion.php
@@ -13,21 +13,27 @@ session_destroy()?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
 </head>
 <body>
-    <div class="container-fluid">
-        <div class="row no-gutter">
-          <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
-          <div class="col-md-8 col-lg-6">
-            <div class="login d-flex align-items-center py-5">
-              <div class="container">
-                <div class="row">
-                  <div class="col-md-9 col-lg-8 mx-auto">
-                    <h3 class="login-heading mb-4">Vous avez été déconnecté !</h3>
-                    <p><a class="text-dark" href="../index">Retour à l'accueil</a></p>
-                </div>
+  <?php 
+    if(!isset($_SESSION['mail'])){
+      header('Location: wrongLink');
+      exit();
+    } 
+  ?>
+  <div class="container-fluid">
+      <div class="row no-gutter">
+        <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+        <div class="col-md-8 col-lg-6">
+          <div class="login d-flex align-items-center py-5">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-9 col-lg-8 mx-auto">
+                  <h3 class="login-heading mb-4">Vous avez été déconnecté !</h3>
+                  <p><a class="text-dark" href="../index">Retour à l'accueil</a></p>
               </div>
             </div>
           </div>
         </div>
-    </div>
+      </div>
+  </div>
 </body>
 </html>
