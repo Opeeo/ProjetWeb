6e24a1e8f3140a0b79bea57dd62904ea6238982f
MAITREPIERRE Thibaut <44748857+SK-Alark@users.noreply.github.com>
2019-11-16 21:40:12 +0100

    Panier

---
+++ ProjetWeb/panierShop.js
@@ -0,0 +1,80 @@
+
+$(document).ready(function() {
+ 
+    /* Taxe & ratio de fade */
+    var tauxTVA = 0.20;
+    var fadeTime = 300;
+     
+     
+    /* Assigne la fonction update ou delete */
+    $('.produit-quantite input').change( function() {
+      updateQuantite(this);
+    });
+     
+    $('.produit-suppr button').click( function() {
+      suppresionProduit(this);
+    });
+     
+     
+    /* Recalcul le panier */
+    function recalculPanier()
+    {
+      var sousTotal = 0;
+       
+      /* Addition de chaque tuples */
+      $('.produit').each(function () {
+        sousTotal += parseFloat($(this).children('.produit-ligne-prix').text());
+      });
+       
+      /* Calcul des totaux */
+      var ttc = sousTotal * tauxTVA;
+      var total = sousTotal + ttc;
+       
+      /* Update totals display */
+      $('.total-valeur').fadeOut(fadeTime, function() {
+        $('#panier-sous-total').html(sousTotal.toFixed(2));
+        $('#panier-tva').html(ttc.toFixed(2));
+        $('#panier-total').html(total.toFixed(2));
+        if(total == 0){
+            $('.paiement').fadeOut(fadeTime);
+        }else{
+            $('.paiement').fadeIn(fadeTime);
+        }
+        $('.total-valeur').fadeIn(fadeTime);
+      });
+    }
+     
+     
+    /* Update quantite */
+    function updateQuantite(quantiteInput)
+    {
+      /* Calcule le prix de ligne-prix */
+      var ligneProduit = $(quantiteInput).parent().parent();
+      var prix = parseFloat(ligneProduit.children('.produit-prix').text());
+      var quantite = $(quantiteInput).val();
+      var lignePrix = prix * quantite;
+       
+      /* Update les ligne-prix et recalcule le total panier */
+      ligneProduit.children('.produit-ligne-prix').each(function () {
+        $(this).fadeOut(fadeTime, function() {
+            $(this).text(lignePrix.toFixed(2));
+            recalculPanier();
+            $(this).fadeIn(fadeTime);
+        });
+      });  
+    }
+     
+    /* Delete le produit du panier sur demande */
+    function suppresionProduit(removeButton)
+    {
+      /* Delete la ligne produit de l'interface & recalcule le total panier */
+      var ligneProduit = $(removeButton).parent().parent();
+      ligneProduit.slideUp(fadeTime, function() {
+        ligneProduit.remove();
+        recalculPanier();
+      });
+    }
+     
+});
+     
+    
---
+++ ProjetWeb/panierShop.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+      <?php include("content/head.php") ?>
+      <link rel="stylesheet" href="assets/css/panierShop.css">
+  </head>
+  <body>
+    <?php include("content/navbar.php") ?>
+    <?php include("content/foot.php") ?>
+      <h1>Panier</h1>
+
+      <div class="panier clearfix">
+        
+        <div class="nom-colonne clearfix">
+          <label class="produit-image">Image</label>
+          <label class="produit-detail">Produit</label>
+          <label class="produit-prix">Prix</label>
+          <label class="produit-quantite">Quantité</label>
+          <label class="produit-suppr">Supprimer</label>
+          <label class="produit-ligne-prix">Total</label>
+        </div>
+        
+        <!-- Produit 1 -->
+        <div class="produit clearfix">
+          <div class="produit-image">
+            <img src="http://placehold.jp/80x120.png">
+          </div>
+          <div class="produit-detail">
+            <div class="produit-nom">Nom</div>
+            <p class="produit-desc">Desc</p>
+          </div>
+          <div class="produit-prix">10.00</div>
+            <div class="produit-quantite">
+              <input type="number" value="1" min="1">
+            </div>
+            <div class="produit-suppr">
+              <button class="produit-suppression btn-danger btn-sm">x</button>
+          </div>
+          <div class="produit-ligne-prix">10.00</div>
+        </div>
+        
+        <!-- Produit 2 -->
+        <div class="produit clearfix">
+          <div class="produit-image">
+            <img src="http://placehold.jp/80x120.png">
+          </div>
+          <div class="produit-detail">
+            <div class="produit-nom">Nom2</div>
+            <p class="produit-desc">Desc2</p>
+          </div>
+          <div class="produit-prix">40.00</div>
+            <div class="produit-quantite">
+              <input type="number" value="1" min="1">
+            </div>
+          <div class="produit-suppr">
+            <button class="produit-suppression btn-danger btn-sm">x</button>
+          </div>
+          <div class="produit-ligne-prix">40.00</div>
+        </div>
+        
+        <!-- Total -->
+        <div class="totals">
+          <div class="total-produits clearfix">
+            <label>Sous-Total</label>
+            <div class="total-valeur" id="panier-sous-total">50.00</div>
+          </div>
+          <div class="total-produits clearfix">
+            <label>TVA (20%)</label>
+            <div class="total-valeur" id="panier-tva">10.00</div>
+          </div>
+          <div class="total-produits clearfix total-produit-total">
+            <label>Total</label>
+            <div class="total-valeur" id="panier-total">60.00</div>
+          </div>
+        </div>
+        <button class="btn btn-success btn-lg btn-block">Paiement</button>
+        
+      </div>
+      <hr>
+    
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="content/panierShop.js"></script>
+  </body>
+</html>
