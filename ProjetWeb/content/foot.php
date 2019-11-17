25e8bce940d8067bb9582e994f662c1f123bf607
MAITREPIERRE Thibaut <44748857+SK-Alark@users.noreply.github.com>
2019-11-17 22:56:14 +0100

    fin juridique

--- ProjetWeb/content/foot.php
+++ ProjetWeb/content/foot.php
@@ -14,7 +14,6 @@
     <div class="text-center text-white bg-black">
         <span><a class="text-white" href="contact.php"><strong>Contact</a> -</strong></span>
         <span><a class="text-white" href="ml.php"><strong>Mentions Légales</a> -</strong></span>
-        <span><a class="text-white" href="cgu.php"><strong>CGU</a> -</strong></span>
         <span><a class="text-white" href="cgv.php"><strong>CGV</a></strong></span>
     </div>
     
